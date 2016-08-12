e2d9c68910519e6d9d80eea2c93bf7d869a2d637
Stas Polushyn <polushyn.stas@gmail.com>
2016-08-12 16:33:45 +0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+new WOW().init();
+$(document).ready(function() {
+$('a[href^="#"]').click(function(){
+var el = $(this).attr('href');
+$('body').animate({
+scrollTop: $(el).offset().top}, 1000);
+return false;
+});
+});
